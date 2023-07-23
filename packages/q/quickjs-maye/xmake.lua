70552847eef725d31d90556f4a16f6dddb424041
maye174 <96584640+maye174@users.noreply.github.com>
2023-07-23 18:12:58 +0800

    fix quickjs

--- packages/q/quickjs-maye/xmake.lua
+++ packages/q/quickjs-maye/xmake.lua
@@ -12,22 +12,19 @@ package("quickjs-maye")
         add_syslinks("dl", "m")
     end
     
-    on_install("linux", "macosx", "iphoneos", "android", "mingw", "cross", "windows", function (package)
+    on_install("linux", "macosx", "iphoneos", "android", "mingw", "cross", function (package)
         io.writefile("xmake.lua", ([[
             add_rules("mode.debug", "mode.release")
-            target("quickjs-maye")
+            target("quickjs")
                 set_kind("$(kind)")
-                add_files("src/quickjs*.c", "src/cutils.c", "src/lib*.c")
-                add_headerfiles("src/quickjs-libc.h")
-                add_headerfiles("src/quickjs.h")
-                add_installfiles("src/*.js", {prefixdir = "share"})
+                add_files("quickjs*.c", "cutils.c", "lib*.c")
+                add_headerfiles("quickjs-libc.h")
+                add_headerfiles("quickjs.h")
+                add_installfiles("*.js", {prefixdir = "share"})
                 set_languages("c99")
                 add_defines("CONFIG_VERSION=\"%s\"", "_GNU_SOURCE")
                 add_defines("CONFIG_BIGNUM")
-                if is_plat("windows") then
-                    add_defines("_MSC_VER")
-                end
-                if is_plat("mingw") then
+                if is_plat("windows", "mingw") then
                     add_defines("__USE_MINGW_ANSI_STDIO")
                 end
         ]]):format(package:version_str()))
@@ -36,19 +33,19 @@ package("quickjs-maye")
             configs.kind = "shared"
         end
         if package:is_plat("cross") then
-            io.replace("src/quickjs.c", "#define CONFIG_PRINTF_RNDN", "")
+            io.replace("quickjs.c", "#define CONFIG_PRINTF_RNDN", "")
         end
         import("package.tools.xmake").install(package, configs)
     end)
 
-    -- on_install("windows", function (package)
-    --     local configs = {}
-    --     if package:config("shared") then
-    --         configs.kind = "shared"
-    --     end
-    --     import("package.tools.xmake").install(package, configs)
-    -- end)
+    on_install("windows", function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("JS_NewRuntime", {includes = "src/quickjs.h"}))
+        assert(package:has_cfuncs("JS_NewRuntime", {includes = "quickjs.h"}))
     end)

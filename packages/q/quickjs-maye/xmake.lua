47c733d8d3ec77293c55b0114939169e6b785214
maye174 <96584640+maye174@users.noreply.github.com>
2023-07-24 01:48:37 +0800

    fix v

--- packages/q/quickjs-maye/xmake.lua
+++ packages/q/quickjs-maye/xmake.lua
@@ -4,7 +4,7 @@ package("quickjs-maye")
     set_description("QuickJS is a small and embeddable Javascript engine")
 
     add_urls("https://github.com/maye174/quickjs.git")
-    add_versions("0.1", "240261f7f748b55e475b321cb57d326fb4a3aaa07b0b97bc1fed8f8510d9815d")
+    add_versions("0.2", "240261f7f748b55e475b321cb57d326fb4a3aaa07b0b97bc1fed8f8510d9815d")
 
     if is_plat("linux", "macosx", "iphoneos", "cross") then
         add_syslinks("pthread", "dl", "m")
